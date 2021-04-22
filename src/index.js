cd7b7667cd5b6b8a9290f384b65e0364703acd3d
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-04-22 12:21:53 +0800

    style: theme, font, and font size responsiveness

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/avenir-roman.woff'; 
 
 ReactDOM.render(
   <React.StrictMode>
