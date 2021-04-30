3d39d8734988d59257bce17a8710bdc2ed644998
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2021-04-30 10:02:54 +0800

    feat: navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './components/Login';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/">
             <Login />
---
+++ src/components/Header.js
@@ -0,0 +1,142 @@
+import styled from "styled-components";
+
+const Header = (props) => {
+  return (
+    <Nav>
+      <Logo>
+        <img src="./images/logo.svg" alt="disney+" />
+      </Logo>
+      <NavMenu>
+        <a href="./home">
+          <img src="./images/home-icon.svg" alt="home" />
+          <span>HOME</span>
+        </a>
+        <a href="./search">
+          <img src="./images/search-icon.svg" alt="home" />
+          <span>SEARCH</span>
+        </a>
+        <a href="./watchlist">
+          <img src="./images/watchlist-icon.svg" alt="home" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="./original">
+          <img src="./images/original-icon.svg" alt="home" />
+          <span>ORIGINAL</span>
+        </a>
+        <a href="./series">
+          <img src="./images/series-icon.svg" alt="home" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <Login>Login</Login>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0px;
+  padding: 0px;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      margin-right: 3px;
+      z-index: auto;
+    }
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        opacity: 0;
+        position: absolute;
+        left: 0px;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
+  /* @media (max-width: 768px) {
+    display: none;
+  } */
+`;
+
+const Login = styled.a`
+  background-color: rgb(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
+
+export default Header;
